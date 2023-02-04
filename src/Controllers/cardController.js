97450c50eaf015cfd202deb8ea8f9090ab0b062b
Juliana Praxedes <julianapraxeds@gmail.com>
2023-02-04 05:15:38 -0300

    Added random Promise to Controller

--- src/Controllers/cardController.js
+++ src/Controllers/cardController.js
@@ -1,33 +1,43 @@
 const Card = require("../Models/card");
 
 const cardController = {
-  create: async (req, res) => {
-    try {
-      const card = {
-        name: req.body.name,
-        kanji: req.body.kanji,
-        katakana: req.body.katakana,
-        clowCardUrl: req.body.clowCardUrl,
-        sakuraCardUrl: req.body.sakuraCardUrl,
-        sign: req.body.sign,
-        hierarchy: req.body.hierarchy,
-        magicType: req.body.magicType,
-        temperament: req.body.temperament,
-        appearance: req.body.appearance,
-        personality: req.body.personality,
-        abilities: req.body.abilities,
-        trivia: req.body.trivia,
-      };
-      const response = await Card.create(card);
-      res.status(201).json(response);
-    } catch (error) {
-      console.log(error);
-    }
-  },
+  // No need for this route, since all canon cards have been added
+  // create: async (req, res) => {
+  //   try {
+  //     const card = {
+  //       name: req.body.name,
+  //       kanji: req.body.kanji,
+  //       katakana: req.body.katakana,
+  //       clowCardUrl: req.body.clowCardUrl,
+  //       sakuraCardUrl: req.body.sakuraCardUrl,
+  //       sign: req.body.sign,
+  //       hierarchy: req.body.hierarchy,
+  //       magicType: req.body.magicType,
+  //       temperament: req.body.temperament,
+  //       appearance: req.body.appearance,
+  //       personality: req.body.personality,
+  //       abilities: req.body.abilities,
+  //       trivia: req.body.trivia,
+  //     };
+  //     const response = await Card.create(card);
+  //     res.status(201).json(response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // },
   getAll: async (req, res) => {
     try {
       const cards = await Card.find();
       res.json(cards);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  random: async (req, res) => {
+    try {
+      const cards = await Card.find();
+      const randomCard = cards[Math.floor(Math.random() * cards.length)];
+      res.json(randomCard);
     } catch (error) {
       console.log(error);
     }
