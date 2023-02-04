20062b6595b974810eff3c7f46b3770ab936a89b
Juliana Praxedes <julianapraxeds@gmail.com>
2023-02-04 05:16:09 -0300

    Added random Promise to Controller

--- src/Routes/services.js
+++ src/Routes/services.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 
 const cardController = require('../Controllers/cardController');
 
-router
-    .route('/cards/addcard')
-    .post((req, res) => cardController.create(req, res));
+// no need for this route, since all canon cards have been added
+// router
+//     .route('/cards/addcard')
+//     .post((req, res) => cardController.create(req, res));
 
-router
-    .route('/cards').get((req, res) => cardController.getAll(req, res));
+router.route('/cards').get((req, res) => cardController.getAll(req, res));
+router.route('/cards/random').get((req, res) => cardController.random(req, res));
 
 module.exports = router;
